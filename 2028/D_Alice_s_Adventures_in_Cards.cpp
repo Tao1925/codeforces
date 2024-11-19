f521a0b091032d1c9fa8ac08d4842a3c4a374646
Tao1925 <yhxxtsy@163.com>
2024-11-19 18:23:47 +0800

    201118

--- 2028/D_Alice_s_Adventures_in_Cards.cpp
+++ 2028/D_Alice_s_Adventures_in_Cards.cpp
@@ -12,8 +12,10 @@ int n;
 const int MA = 2e5+3;
 int dat[3][MA];
 int ind[3][MA];
+bool end_flag = false;
 vector<int> cur_max(3);
-int av[MA];
+vector<int> tar(3);
+int vis[3][MA];
 stack<pair<int, int>> st;
 stack<pair<int, int>> ans;
  
@@ -24,11 +26,38 @@ char to_c(int a){
     return 'a';
 }
  
-void dfs(int cur){
-    if (av[cur] == 1) return;
-    av[cur] = 1;
-    if (cur == n - 1) {ans = st; return;}
-    vector<int> ccur_max(cur_max);
+void dfs(int usr, int cur){
+    cout << "dfs(" << usr << "," << cur << ")" << endl;
+    if (end_flag) return;
+    if (vis[usr][cur] == 1) return;
+    vis[usr][cur] = 1;
+
+    if (dat[usr][cur] > tar[usr]){
+        st.push(make_pair(usr, n));
+        ans = st;
+        end_flag = true;
+        return;
+    }
+
+    int cur_data = dat[usr][cur];
+    if (cur_data < cur_max[usr])
+        return;
+    
+
+    for (int i = cur_max[usr] + 1; i < cur_data; i++){
+        int index = ind[usr][i];
+        if (index < cur) continue;
+        for (int j = 0; j < 3; j++){
+            if (j == usr) continue;
+            st.push(make_pair(usr, index));
+            dfs(j, index);
+            st.pop();
+        }
+    }
+
+    cur_max[usr] = cur_data;
+
+    /*
     for (int i = 0; i < 3; i++){
         // cout << "cur = " << cur << " i = " << i << endl;
         if (dat[i][cur] <= ccur_max[i]) continue;
@@ -43,6 +72,7 @@ void dfs(int cur){
         }
         cur_max[i] = max(dat[i][cur], cur_max[i]);
     }
+    */
 }
  
 void solve(){
@@ -54,12 +84,20 @@ void solve(){
             ind[i][d] = j;
         }
     }
-    for (int i = 0; i < n; i++) av[i] = 0;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < n; j++)
+             vis[i][j] = 0;
     for (int i = 0; i < 3; i++) cur_max[i] = -1;
-    dfs(0);
-    if (av[n - 1] == 1){
+    for (int i = 0; i < 3; i++) tar[i] = dat[i][n - 1];
+
+    dfs(0, 0);
+    dfs(1, 0);
+    dfs(2, 0);
+
+    if (end_flag){
         cout << "YES" << endl;
-        vector<pair<char, int>> st_r;
+        vector<pair<int, int>> st_r;
         while(!ans.empty()){
             st_r.push_back(ans.top());
             ans.pop();
@@ -75,10 +113,10 @@ int main()
 {
 
     #ifdef LOCAL
-        cout << "hello local" << endl;
+        cout << std::filesystem::current_path()  << endl;
         // 本地调试时使用文件输入输出
-        ifstream infile("../io/data.in");
-        ofstream outfile("../io/data.out");
+        ifstream infile("../../io/data.in");
+        ofstream outfile("../../io/data.out");
         cout << "hello in&out" << endl;
         // 检查文件是否成功打开
         if (!infile || !outfile) {
